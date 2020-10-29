f97f4674a12364274a894b5328c006208dc41eae
regnull <regnull@gmail.com>
2020-10-28 21:59:36 -0400

    Create IVPercentile.ts

---
+++ scripts/IVPercentile.ts
@@ -0,0 +1,22 @@
+# IVPercentile
+# This study shows the current IV percentile, 0 to 100.
+#
+# Copyright (c) 2020 Leonid Gorkin
+# regnull@gmail.com
+#
+# MIT Licence https://opensource.org/licenses/MIT
+declare lower;
+declare hide_on_intraday;
+
+def notDaily = if GetAggregationPeriod() <> AggregationPeriod.DAY then yes else no;
+AddLabel(notDaily, "This study works on daily charts only");
+
+def iv0 = imp_volatility();
+def iv = if !IsNaN(iv0) then iv0 else iv0[-1];
+
+def min = Lowest(iv, 252);
+def max = Highest(iv, 252);
+
+def pct = (iv - min)/(max - min);
+
+plot IVPercentile = pct * 100;
