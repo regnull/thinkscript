0935b35f35038d8c0b426b706b67921190416b8f
regnull <regnull@gmail.com>
2020-10-28 22:39:39 -0400

    Update IVPercentile.ts

--- scripts/IVPercentile.ts
+++ scripts/IVPercentile.ts
@@ -8,14 +8,16 @@
 declare lower;
 declare hide_on_intraday;
 
+input length = 252;
+
 def notDaily = if GetAggregationPeriod() <> AggregationPeriod.DAY then yes else no;
 AddLabel(notDaily, "This study works on daily charts only");
 
 def iv0 = imp_volatility();
 def iv = if !IsNaN(iv0) then iv0 else iv0[-1];
 
-def min = Lowest(iv, 252);
-def max = Highest(iv, 252);
+def min = Lowest(iv, length);
+def max = Highest(iv, length);
 
 def pct = (iv - min)/(max - min);
 
