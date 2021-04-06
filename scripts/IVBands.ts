7b46ff0118056867a972aea4e429830489c6a1bd
regnull <regnull@gmail.com>
2021-04-06 16:12:37 -0400

    Update IVBands.ts

--- scripts/IVBands.ts
+++ scripts/IVBands.ts
@@ -8,15 +8,38 @@
 # MIT Licence https://opensource.org/licenses/MIT
 
 input length = 20;
-# To get 30% probability, we need to divide sigma by two.
+
+# Sigma factor depends on what kind of options we are modeling.
+# We compute it from the probability we want (which roughly equals option delta) 
+# using cumulative normal distribution function. 
+# Unfortunately Thinkscript doesn't have cumululative normal distribution function, so
+# here are some numbers to translate probabilities into sigma factors:
+
+# Probability (Option delta)    Sigma factor
+# 0.4                           0.25
+# 0.3                           0.5
+# 0.2                           0.8
+# 0.1                           1.25
+
+# Let's say we use sigma factor of 0.5, which translates into option delta of 0.3.
+# If we were to sell a call with 0.3 delta 20 days ago, and the price today is below
+# the top band, the option would not get assigned. If the price is above the top band,
+# the option would get assigned.
+
+# Same with puts and the bottom band.
+
 input sigmaFactor = 0.5;
+
+# Averaging length, just to make chart less jumpy.
+
+input avg_length = 5;
 
 def iv0 = imp_volatility();
 def iv = if !IsNaN(iv0) then iv0 else iv0[-1];
 
 def ivPast = iv[length]*Sqrt(length/365)*sigmaFactor;
-def upperBand = close[length]*(1 + ivPast);
-def lowerBand = close[length]*(1 - ivPast);
+def upperBand = MovingAverage(AverageType.Simple, close[length]*(1 + ivPast), avg_length);
+def lowerBand =  MovingAverage(AverageType.Simple, close[length]*(1 - ivPast), avg_length);
 
 plot UpperBandP = upperBand;
 plot LowerBandP = lowerBand;
