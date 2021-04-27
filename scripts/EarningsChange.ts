2d33ea27d3c0dc9662ff46a3c2d844ea70525096
regnull <regnull@gmail.com>
2021-04-27 08:13:41 -0400

    Create EarningsChange.ts

---
+++ scripts/EarningsChange.ts
@@ -0,0 +1,21 @@
+# IVBAnads
+# 
+# Shows bands separated by IV-implied stock movement as of some number of days ago.
+#
+# Copyright (c) 2020 Leonid Gorkin
+# regnull@gmail.com
+#
+# MIT Licence https://opensource.org/licenses/MIT
+
+def afterEarnings = GetEventOffset(Events.Earnings, -1);
+
+def delta = (open - close[1]) / close[1];
+def change;
+
+if afterEarnings == 1 {
+    change = delta;
+} else {
+    change = change[1];
+}
+
+plot ChangePlot = change * 100;
